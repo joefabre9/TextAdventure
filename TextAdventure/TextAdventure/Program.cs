826d2ba039b5e0f4760d14fdc14a053a14037381
Joe Fabre <joefabre@Joes-MacBook-Pro.local>
2020-11-10 21:22:39 -0500

    111020202122 Minor chances

--- TextAdventure/TextAdventure/Program.cs
+++ TextAdventure/TextAdventure/Program.cs
@@ -17,7 +17,7 @@ namespace Adventure
         static string CharacterName;
 
         static string[] PartOne = {
-            "At the front of the imposing building you see a weathered old man with a cart.\nAs you near, you see the cart is filled with what looks like mostly junk and \nonly a few useful items. All you have on you is piece of a chalk.\nYou offer it to him, and he says he'll trade a flashlight or an umbrella for it.\nTo choose type either A for the flashlight, or B for the umbrella.",
+            "At the front of the swank building you see a tired old man with a buggy.\nAs you near, you see the buggy is filled with what looks like mostly junk and only a few useful items. All you have on you is piece of a candy.\nYou offer it to him, and he says he'll trade a flashlight or an umbrella for it.\nTo choose type either A for the flashlight, or B for the umbrella.",
             "The power in the building goes out - luckily you have a flashlight! \nYou move the light around and a large animal is frightened by the \nsudden brightness and takes off. As you move the light again, \nsomething glitters. You reach down and pick up a coin!",
             "The power in the building goes out! As you move down the hallway \nyou hear what sounds like a large animal nearby. You move the \numbrella in a widening arc in front of you to scare it, \nand the animal skitters off.",
             "The lights return and you move into a room at the end of the hall. \nThere is a vending machine.",
